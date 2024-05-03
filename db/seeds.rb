1470c1936f52bcc9752f26ce870310312651466b
gabrielZZ231 <gabriel1235897@gmail.com>
2024-05-03 00:10:54 -0300

    Commit inicial do projeto

---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+User.create!(name:  "Example User",
+             email: "example@bor.com",
+             password:              "123",
+             password_confirmation: "123")
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@bowr.com"
+  password = "123"
+  User.create!(name: name,
+               email: email,
+               password:password,
+               password_confirmation: password)
+end
+
